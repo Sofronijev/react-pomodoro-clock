ffc29760cf5704247f484c35ab0240149d96340c
Sofronijev <sofronijev@gmail.com>
2020-03-08 13:19:25 +0100

    Updates

---
+++ precache-manifest.69c3ac63ac0d1e1e984ec60cb01da9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26215e6d523b8f44cb387c966c516229",
+    "url": "/react-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "e718f6f7f6a54a53e764",
+    "url": "/react-pomodoro-clock/static/css/main.6c689201.chunk.css"
+  },
+  {
+    "revision": "17582dd1e5f9eb23ba55",
+    "url": "/react-pomodoro-clock/static/js/2.6acf203c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pomodoro-clock/static/js/2.6acf203c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e718f6f7f6a54a53e764",
+    "url": "/react-pomodoro-clock/static/js/main.372b428b.chunk.js"
+  },
+  {
+    "revision": "0964364ed55c49331c0b",
+    "url": "/react-pomodoro-clock/static/js/runtime-main.06a7577d.js"
+  },
+  {
+    "revision": "10737d2884c45a81d519a79b7cb2e83a",
+    "url": "/react-pomodoro-clock/static/media/bell.10737d28.mp3"
+  }
+]);
